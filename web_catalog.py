d986ae118b50ff3f8a0e4084ae26ebdffb753940
GabrielKni <39495594+GabrielKni@users.noreply.github.com>
2022-08-19 12:04:17 -0400

    Update web_catalog.py
    get a connection and data into a dataframe

--- web_catalog.py
+++ web_catalog.py
@@ -1,4 +1,20 @@
 import streamlit
 import snowflake.connector
+import pandas
 
 streamlit.title("Zena's Amazing Athleisure Catalog")
+
+# connect to snowflake
+conn = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+cur = conn.cursor()
+
+# run a snwoflake query and put it all in a car called catalog
+select_str = """Select color_or_style from catalog_for_website"""
+cur.execute(select_str)
+catalog = cur.fetchall()
+
+#put the data into a dataframe
+df = pandas.DataFrame(catalog)
+
+# testing dataframe data
+streamlit.write(df)
